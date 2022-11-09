08920ae8ad791b646a9c1814b579b4d5deab41c8
daya <dayajuangmahaputra@gmail.com>
2022-11-09 20:30:43 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,23 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printFactors(10);
+    }
+
+    public static void printFactors(int n){
+        // Check if n is greater than 1 (we don't want any 0 or negative input)
+        if(n < 1)
+            System.out.println("Invalid Value");
+
+        // Check the factor of n
+        int factor = 1; // initiate the factor
+
+        while(factor <= n){
+            // Check if the factor has remainder or not
+            if(n % factor == 0)
+                System.out.println(factor);
+            factor++;
+        }
     }
 }
